3dadd479bdce4d0c69b3bf9174031baadde41491
narenramakrishnan5 <74294122+narenramakrishnan5@users.noreply.github.com>
2022-05-17 22:33:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCItKnr15n3jOvhtjmzkD7MDyNXTt0UhzU",
+  authDomain: "noticeboard-37d27.firebaseapp.com",
+  projectId: "noticeboard-37d27",
+  storageBucket: "noticeboard-37d27.appspot.com",
+  messagingSenderId: "624844198411",
+  appId: "1:624844198411:web:7bd79caa6a0b27f4c1ad0e"
+};
